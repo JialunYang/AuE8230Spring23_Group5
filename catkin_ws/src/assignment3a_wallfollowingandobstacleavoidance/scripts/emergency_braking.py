e349f0637b24d39c7f073e959ac09d9d22e6367a
Anirudh-Kyatham <114123968+Anirudh-Kyatham@users.noreply.github.com>
2023-03-06 18:38:33 -0500

    Update emergency_braking.py

--- catkin_ws/src/assignment3a_wallfollowingandobstacleavoidance/scripts/emergency_braking.py
+++ catkin_ws/src/assignment3a_wallfollowingandobstacleavoidance/scripts/emergency_braking.py
@@ -6,13 +6,13 @@ from geometry_msgs.msg import Twist
 
 def scan_callback(data):
     scan_value = data.ranges[len(data.ranges)//2] # get the center-most value of the scan data array
-    threshold = 0.5 # define the distance threshold for emergency braking
+    threshold = 0.5 # The distance threshold for emergency braking
 
     if scan_value < threshold:
         move.linear.x = 0 # publish 0 to /cmd_vel if the robot gets too close to the wall
         move.angular.z = 0
     else:
-        move.linear.x = 0.2     # High Speed
+        move.linear.x = 0.2     # Low Speed
         #move.linear.x = 0.4    # Medium Speed
         #move.linear.x = 0.6    # High Speed
         move.angular.z = 0
