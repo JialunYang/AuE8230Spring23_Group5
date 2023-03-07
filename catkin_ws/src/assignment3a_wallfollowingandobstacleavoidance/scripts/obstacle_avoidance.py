ed4cf49e371aa65f217f1a085bcfb85b1646bd6c
Anirudh-Kyatham <114123968+Anirudh-Kyatham@users.noreply.github.com>
2023-03-07 09:01:53 -0500

    Update obstacle_avoidance.py

--- catkin_ws/src/assignment3a_wallfollowingandobstacleavoidance/scripts/obstacle_avoidance.py
+++ catkin_ws/src/assignment3a_wallfollowingandobstacleavoidance/scripts/obstacle_avoidance.py
@@ -9,7 +9,7 @@ class WallFollowing:
         self.move = Twist()
         self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
         self.sub = rospy.Subscriber("/scan", LaserScan, self.wallfollow)
-        self.thresh = 0.4
+        self.thresh = 0.25   # Thresold
 
     def wallfollow(self, data):
         print('Range at 20 degress: {}'.format(data.ranges[20]))
@@ -22,31 +22,31 @@ class WallFollowing:
         if (data.ranges[20] > self.thresh and 
             data.ranges[340] > self.thresh and 
             data.ranges[0] > self.thresh):
-            self.move.linear.x = 0.5
+            self.move.linear.x = 0.4
             self.move.angular.z = 0
         elif (data.ranges[20] > self.thresh and 
               data.ranges[55] > self.thresh and 
               data.ranges[90] > self.thresh):
             self.move.linear.x = 0.0
-            self.move.angular.z = 0.155
+            self.move.angular.z = 1.0     #Right
             if (data.ranges[0] > self.thresh and 
                 data.ranges[20] > self.thresh and 
                 data.ranges[340] > self.thresh):
-                self.move.linear.x = 0.5
+                self.move.linear.x = 0.4
                 self.move.angular.z = 0.0
         elif (data.ranges[270] > self.thresh and 
               data.ranges[305] > self.thresh and 
               data.ranges[340] > self.thresh):
             self.move.linear.x = 0.0
-            self.move.angular.z = 0.85
+            self.move.angular.z = -1      #Left
             if (data.ranges[0] > self.thresh and 
                 data.ranges[20] > self.thresh and 
                 data.ranges[340] > self.thresh):
-                self.move.linear.x = 0.5
+                self.move.linear.x = 0.4
                 self.move.angular.z = 0.0
         else:
-            self.move.linear.x = 0.0
-            self.move.angular.z = 0.5
+            self.move.linear.x = 0.0           #Checked for other values
+            self.move.angular.z = 0.5          # Used negative and postive but positive gave us good results
 
         self.pub.publish(self.move)
 
