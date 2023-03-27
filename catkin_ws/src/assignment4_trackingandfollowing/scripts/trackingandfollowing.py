b2ec8fa90aaabf7f96b6c0648994b93bff8ca4ee
krishnasainagineni07 <122739282+krishnasainagineni07@users.noreply.github.com>
2023-03-27 15:46:44 -0400

    Update trackingandfollowing.py

--- catkin_ws/src/assignment4_trackingandfollowing/scripts/trackingandfollowing.py
+++ catkin_ws/src/assignment4_trackingandfollowing/scripts/trackingandfollowing.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import rospy
 import cv2
 import numpy as np
@@ -7,79 +6,72 @@ from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
 from move_robot import MoveTurtlebot3
+from time import time
 
-class LineFollower:
-    def __init__(self, camera_topic):
-        # Initialize the ROS node
-        rospy.init_node('line_follower_node')
+class LineFollower(object):
 
-        # Subscribe to the camera feed
-        self.image_sub = rospy.Subscriber(camera_topic, Image, self.camera_callback)
-
-        # Create a CvBridge object to convert ROS images to OpenCV images
-        self.bridge = CvBridge()
-
-        # Create an instance of MoveTurtlebot3 to control the robot's movement
-        self.robot = MoveTurtlebot3()
+    def __init__(self):
+        self.bridge_object = CvBridge()
+        self.image_sub = rospy.Subscriber("/raspicam_node/image",Image,self.camera_callback)
+        self.moveTurtlebot3_object = MoveTurtlebot3()
 
     def camera_callback(self, data):
-        try:
-            # Convert the ROS image to OpenCV format
-            cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
-        except CvBridgeError as e:
-            print(e)
-            return
-
-        # Crop the image to focus only on the area where the line is expected
+        cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
         height, width, channels = cv_image.shape
-        roi = cv_image[int(height*0.5):height, int(width*0.25):int(width*0.75)]
+        crop_img = cv_image[int((height/2)+150):int((height/2)+170)][1:int(width)]
+        hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+        mask = self.threshold_image(hsv)
 
-        # Convert the image to HSV color space
-        hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+        cx, cy = self.calculate_centroid(mask, height, width)
 
-        # Threshold the image to extract the yellow line
-        lower_yellow = np.array([20, 100, 100])
-        upper_yellow = np.array([50, 255, 255])
+        twist_object = self.calculate_control_command(cx, width)
+
+        self.moveTurtlebot3_object.move_robot(twist_object)
+        
+    def threshold_image(self, hsv):
+        lower_yellow = np.array([20,100,100])
+        upper_yellow = np.array([50,255,255])
         mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        return mask
 
-        # Calculate the centroid of the yellow line
-        M = cv2.moments(mask)
-        if M['m00'] == 0:
-            # The line is not detected
-            return
-        cx = int(M['m10']/M['m00'])
-        cy = int(M['m01']/M['m00'])
+    def calculate_centroid(self, mask, height, width):
+        m = cv2.moments(mask, False)
+        try:
+            cx, cy = m['m10']/m['m00'], m['m01']/m['m00']
+        except ZeroDivisionError:
+            cx, cy = height/2, width/2
+        return cx, cy
+        
+    def calculate_control_command(self, cx, width):
+        global cxlast, lasttime
+        twist_object = Twist()
+        twist_object.linear.x = 0.22
+        p = max(-0.15, min(0.15, ((width/2) - cx)/3000))
+        d = max(-0.15, min(0.15, (cxlast-cx)/(time() - lasttime)*1.2*1e7))
+        twist_object.angular.z = p+d
+        cxlast = cx
+        lasttime = time()
+        return twist_object
 
-        # Draw a circle at the centroid of the yellow line
-        cv2.circle(roi, (cx, cy), 10, (0, 0, 255), -1)
-
-        # Calculate the error between the centroid and the center of the image
-        err = cx - width/2
-
-        # Create a Twist message to control the robot's movement
-        twist = Twist()
-
-        # Set the linear speed to a constant value
-        twist.linear.x = 0.3
-
-        # Set the angular speed proportional to the error
-        twist.angular.z = -float(err) / 1000
-
-        # Send the movement command to the robot
-        self.robot.move_robot(twist)
-
-        # Show the image with the line detected
-        cv2.imshow('Line', roi)
-        cv2.waitKey(1)
-
-    def run(self):
-        # Start the main loop
-        rospy.spin()
-
-    def shutdown(self):
-        # Stop the robot's movement and destroy any OpenCV windows
-        self.robot.stop_robot()
+    def clean_up(self):
+        self.moveTurtlebot3_object.clean_class()
         cv2.destroyAllWindows()
 
-if __name__ == '__main
+def main():
+    rospy.init_node('line_following_node', anonymous=True)
+    line_follower_object = LineFollower()
+    rate = rospy.Rate(5)
+    ctrl_c = False
+    def shutdownhook():
+        line_follower_object.clean_up()
+        rospy.loginfo("Shutdown time!")
+        ctrl_c = True
+    rospy.on_shutdown(shutdownhook)
+    while not ctrl_c:
+        rate.sleep()
 
+if __name__ == '__main__':
+    lasttime = 0
+    cxlast = 0
+    main()
+``
