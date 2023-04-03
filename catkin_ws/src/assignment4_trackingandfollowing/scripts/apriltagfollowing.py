9a8439705165618396c3cf7dad3f689f9f7c0a22
Anirudh-Kyatham <114123968+Anirudh-Kyatham@users.noreply.github.com>
2023-04-03 12:42:53 -0700

    Add files via upload

--- catkin_ws/src/assignment4_trackingandfollowing/scripts/apriltagfollowing.py
+++ catkin_ws/src/assignment4_trackingandfollowing/scripts/apriltagfollowing.py
@@ -1,45 +1,58 @@
 #!/usr/bin/env python3
 
 import rospy
-from geometry_msgs.msg import Twist
+import cv2
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
 from apriltag_ros.msg import AprilTagDetectionArray
+from geometry_msgs.msg import Twist
 
-
-class AprilTagFollower():
-
+class AprilTagFollower:
     def __init__(self):
+        # Initialize the ROS node
+        rospy.init_node('april_tag_follower_node', anonymous=True)
+        
+        # Publish to velocity control
+        self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        
+        # Subscribe to the AprilTag detections
+        self.tag_sub = rospy.Subscriber('/tag_detections', AprilTagDetectionArray, self.tag_callback)
+
+        # Other init
+        self.rate = rospy.Rate(10)
         self.x = 0
         self.z = 0
-        self.k_linear_vel = 0.4
-        self.k_angular_vel = 1
-        # initializing a node
-        rospy.init_node('AprilTagFollower', anonymous=True)
-        # defining publisher
-        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-        # defining subscriber
-        self.sub = rospy.Subscriber(
-            '/tag_detections', AprilTagDetectionArray, self.pose)
-        # subscriber -Apriltagdetection array callback function that stores the tag detection array coordinates in xyz world frame
-        self.rate = rospy.Rate(10)
 
-    def pose(self, data):
-        # extracting pose of april tag
-        self.x = data.detections[0].pose.pose.pose.position.x
-        self.z = data.detections[0].pose.pose.pose.position.z
-        print("The front distance is :", self.x)
-        print("The header is :", self.z)
+    def tag_callback(self, data):
+        tag = data.detections[0]
+        tag_id = tag.id[0]
+        self.x = tag.pose.pose.pose.position.x
+        self.z = tag.pose.pose.pose.position.z
+        self.y = tag.pose.pose.pose.position.y
+               
+        # Print the tag information
+        rospy.loginfo('Detected tag %d at position (%.2f, %.2f, %.2f)' % (tag_id, self.x, self.y, self.z))
 
-    def april_tag_follower(self):
+    def run(self):
         self.vel_msg = Twist()
+        kx = 0.2
+        kz = -2
         while not rospy.is_shutdown():
-            # velocity controller
-            self.vel_msg.linear.x = self.z*self.k_linear_vel
-            self.vel_msg.angular.z = -self.x*self.k_angular_vel
-            # publishing velocity
-            self.pub.publish(self.vel_msg)
+            # Control and publish the velocity
+            if self.z >=0.4:
+                self.vel_msg.linear.x = self.z*kx
+                self.vel_msg.angular.z = self.x*kz
+            elif self.z <= 0.3:
+                self.vel_msg.linear.x = -self.z*kx
+                self.vel_msg.angular.z = -self.x*kz
+            else:
+                self.vel_msg.linear.x = 0
+                self.vel_msg.angular.z = 0
+                                
+            self.vel_pub.publish(self.vel_msg)
             self.rate.sleep()
 
-
 if __name__ == '__main__':
-    tagfollow = AprilTagFollower()
-    tagfollow.april_tag_follower()
+    follower = AprilTagFollower()
+    follower.run()
+
